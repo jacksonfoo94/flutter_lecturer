125448a1146b2dd22eda3a0add3b287377b3cc80
Jackson Foo <ks.foo@trinitywizards.com>
2024-01-01 21:26:57 +0800

    [features] - added list view layout

--- lib/presentation/pages/dummy_ui.dart
+++ lib/presentation/pages/dummy_ui.dart
@@ -5,26 +5,51 @@ import 'package:trinity_lecture_app/presentation/widgets/molecules/action_text.d
 import 'package:trinity_lecture_app/presentation/widgets/organisms/ui_helper.dart';
 
 import '../widgets/molecules/platform_app_bar.dart';
+import '../widgets/organisms/news_card.dart';
 
 @RoutePage()
 class DummyUIPage extends StatelessWidget {
   static var page;
 
   const DummyUIPage({super.key});
-  // const DummyUIRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const PlatformAppBar(
-        title: Text(
-          'Dummy UI',
+    return DefaultTabController(
+        length: 3,
+        child : Scaffold(
+        appBar: const PlatformAppBar(
+          title: Text(
+            'Dummy UI',
+          ),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "List View"),
+              Tab(text: "Grid View"),
+            ],
+          ),
         ),
-      ),
-      body: Padding(
-        padding: UIHelper.padding(horizontal: 20, vertical: 24),
-        child: Text("data"),
-      ),
+        body: TabBarView(
+          children: [
+            ListView.builder(
+                itemCount: Constants.homePageListing.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: UIHelper.padding(vertical: 10, horizontal: 24),
+                    child: NewsCard(imgSrc: Constants.dummyImg, title: "How can I be flutter developer Expert", desc: "Jill Lepore . 23 May 2023")
+                  );
+                }
+            ),
+            ListView.builder(
+                itemCount: Constants.homePageListing.length,
+                itemBuilder: (context, index) {
+                  return ActionText(onTap: () {
+                  }, title: Constants.homePageListing[index].title, desc: Constants.homePageListing[index].description);
+                }
+            )
+          ],
+        )
+      )
     );
   }
 }
